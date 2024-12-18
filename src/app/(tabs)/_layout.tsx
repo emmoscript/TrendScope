58253d38ff4cba78ee84929ab19decd0d9147474
Emil0309 <71798519+Emil0309@users.noreply.github.com>
2024-12-18 19:34:18 -0400

    adding example data

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -29,8 +29,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Stocks',
+          tabBarIcon: ({ color }) => <TabBarIcon name="area-chart" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
               <Pressable>
---
+++ src/components/StockListItem.tsx
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+import { MonoText } from "./StyledText";
+import { Text, View } from "./Themed";
+import Colors from "../constants/Colors";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
+type Stock = {
+    name: string;
+    symbol: string;
+    close: string;
+    percent_change: string;
+}
+
+type StockListItem = {
+    stock: Stock;
+}
+
+export default function StockListItem({stock}: StockListItem) {
+  
+  const change = Number.parseFloat(stock.percent_change);
+
+  return (
+    <View style={styles.container}>
+        
+        {/* Left Container */}
+        <View style={{flex: 1, gap: 5}}>
+            <Text style={styles.symbol}>
+                {stock.symbol} 
+                {'  '}
+                <FontAwesome name="star-o" size={18} color="gray" />
+            </Text>
+            <Text style={{color: 'gray'}}>{stock.name}</Text>
+        </View>
+        
+        {/* Right Container */}
+        <View style={{alignItems: 'flex-end'}}>
+            <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+            <MonoText style={{color: change > 0 ? 'green' : 'red'}}>
+                {change > 0 ? '+' : ''}
+                {change.toFixed(1)}%
+            </MonoText>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  symbol: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+  },
+});
