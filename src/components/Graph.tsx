a26a5c0bbfae87f40f350a62f28df7218e3738cf
Emil0309 <71798519+Emil0309@users.noreply.github.com>
2025-01-22 07:42:37 -0400

    Graph Integration

---
+++ src/components/Graph.tsx
@@ -0,0 +1,51 @@
+import Colors from '../constants/Colors'
+import { View, Text } from './Themed'
+import { GraphPoint, LineGraph } from 'react-native-graph'
+import timeseries from '@/assets/data/timeseries.json'
+import { MonoText } from './StyledText'
+import { useState } from 'react'
+
+const Graph = () => {
+
+  const points: GraphPoint[] = timeseries.values.map((value) => ({
+    date: new Date(value.datetime), 
+    value: Number.parseFloat(value.close),
+  }));
+
+  const [selectedPoint, setSelectedPoint] = useState<GraphPoint>(
+    points[points.length - 1]
+  );
+
+  const onPointSelected = (point: GraphPoint) => {
+    setSelectedPoint(point);
+  };
+
+  return (
+    <View>
+
+      <MonoText style={{fontSize: 20, fontWeight: 'bold', color: '#017560'}}>
+        ${selectedPoint?.value.toFixed(1)}
+      </MonoText>
+      <Text style={{ color: 'gray' }}>
+        {selectedPoint?.date.toDateString()}
+      </Text>
+      
+
+      <LineGraph
+        style={{width:'100%' ,height: 300}}
+        points={points}
+        animated={true}
+        color="#017560"
+        gradientFillColors={['#0175605D', '#7476df00']}
+        enablePanGesture
+        onPointSelected={onPointSelected}
+        onGestureEnd={() => setSelectedPoint(points[0])}
+        enableIndicator
+        enableFadeInMask
+        indicatorPulsating
+      />
+    </View>
+  )
+}
+
+export default Graph
